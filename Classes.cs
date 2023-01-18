67002a162a9ce686710f8749c45e6f4e54ccffe5
Johannes Lindblom <johannes.lind@hotmail.se>
2023-01-18 16:32:07 +0100

    :)

---
+++ Classes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Pong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Pong
+{
+    internal class Classes
+    {
+        public void KeyMovements()
+        {
+
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,11 @@ namespace Pong
 {
     public class Game1 : Game
     {
+
+        Classes Class = new Classes();
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D platform;
         Texture2D myship;
         Texture2D coin;
         Texture2D tripod;
@@ -20,12 +23,14 @@ namespace Pong
         List<Vector2> coin_pos_list = new List<Vector2>();
         List<Vector2> tripod_pos_list = new List<Vector2>();
         Rectangle rec_myship;
-        Rectangle rec_coin; 
+        Rectangle rec_coin;
+        Rectangle platformVis;
+        
         SpriteFont gameFont;
 
 
 
-
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -52,7 +57,6 @@ namespace Pong
                 tripod_pos_list.Add(tripod_pos);
             }
 
-
             // TODO: Add your initialization logic here
             myship_pos.X = 100;
             myship_pos.Y = 100;
@@ -72,18 +76,55 @@ namespace Pong
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            gameFont = Content.Load<SpriteFont>("Utskrift/GameFont2");
 
             // TODO: use this.Content to load your game content here
             coin = Content.Load<Texture2D>("Sprites/coin");
             myship = Content.Load<Texture2D>("Sprites/ship");
             tripod = Content.Load < Texture2D>("Sprites/tripod");
+
+            platform = new Texture2D(GraphicsDevice, 1, 1);
+            platform.SetData(new Color[] { Color.Black });
+
         }
 
+        public void KeyInput()
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.D))
+                myship_pos.X = myship_pos.X + myship_speed.X;
+            if (keyboardState.IsKeyDown(Keys.A))
+                myship_pos.X = myship_pos.X - myship_speed.X;
+
+            if (keyboardState.IsKeyDown(Keys.S))
+                myship_pos.Y = myship_pos.Y + myship_speed.Y;
+            if (keyboardState.IsKeyDown(Keys.W))
+                myship_pos.Y = myship_pos.Y - myship_speed.Y;
+
+
+            if (((myship_pos.Y == Window.ClientBounds.Height - myship.Height) && keyboardState.IsKeyDown(Keys.S) == true) || (myship_pos.Y == 0 && keyboardState.IsKeyDown(Keys.W)))
+            {
+                myship_speed.Y = 0f;
+            }
+            else if (((myship_pos.Y == Window.ClientBounds.Height - myship.Height) && keyboardState.IsKeyDown(Keys.W) == true) || (myship_pos.Y == 0 && keyboardState.IsKeyDown(Keys.S) == true))
+            {
+                myship_speed.Y = 4f;
+            }
+
+            if (((myship_pos.X == Window.ClientBounds.Width - myship.Width) && keyboardState.IsKeyDown(Keys.D) == true) || (myship_pos.X == 0 && keyboardState.IsKeyDown(Keys.A) == true))
+            {
+                myship_speed.X = 0f;
+            }
+            else if (((myship_pos.X == Window.ClientBounds.Width - myship.Width) && keyboardState.IsKeyDown(Keys.A) == true) || (myship_pos.X == 0 && keyboardState.IsKeyDown(Keys.D) == true))
+            {
+                myship_speed.X = 4f;
+            }
+
+        }
         public void EnemySPeed()
         {
             for (int i = 0; i < tripod_pos_list.Count; i++)
             {
-
                 tripod_pos_list[i] = tripod_pos_list[i] + tripod_speed;
             }
         }
@@ -112,53 +153,16 @@ namespace Pong
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
+           
 
             // TODO: Add your update logic here
-                EnemySPeed();
+                
             //BoundaryCheckEn();
 
-            for (int i = 0; i < tripod_pos_list.Count; i++)
-            {
-                if (tripod_pos_list[i].Y >= (Window.ClientBounds.Height + tripod.Height))
-                {
-                    tripod_pos_list.RemoveAt(i);
-                }
-            }
-
+            EnemySPeed();
             CheckCollision();
-
-
-
-
-            KeyboardState keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.D))
-                myship_pos.X = myship_pos.X + myship_speed.X;
-            if (keyboardState.IsKeyDown(Keys.A))
-                myship_pos.X = myship_pos.X - myship_speed.X;
-
-            if (keyboardState.IsKeyDown(Keys.S))
-                myship_pos.Y = myship_pos.Y + myship_speed.Y;
-            if (keyboardState.IsKeyDown(Keys.W))
-                myship_pos.Y = myship_pos.Y - myship_speed.Y;
-            
-
-            if (((myship_pos.Y == Window.ClientBounds.Height - myship.Height) && keyboardState.IsKeyDown(Keys.S) == true) || (myship_pos.Y == 0 && keyboardState.IsKeyDown(Keys.W)))
-            {
-                myship_speed.Y = 0f;
-            }
-            else if (((myship_pos.Y == Window.ClientBounds.Height - myship.Height) && keyboardState.IsKeyDown(Keys.W)==true) || (myship_pos.Y == 0 && keyboardState.IsKeyDown(Keys.S) == true))
-            {
-                myship_speed.Y = 4f;
-            }
-
-            if (((myship_pos.X == Window.ClientBounds.Width - myship.Width) && keyboardState.IsKeyDown(Keys.D) == true) || (myship_pos.X == 0 && keyboardState.IsKeyDown(Keys.A) == true))
-            {
-                myship_speed.X = 0f;
-            }
-            else if (((myship_pos.X == Window.ClientBounds.Width - myship.Width) && keyboardState.IsKeyDown(Keys.A) == true) || (myship_pos.X == 0 && keyboardState.IsKeyDown(Keys.D) == true))
-            {
-                myship_speed.X = 4f;
-            }
+            KeyInput();
 
 
             base.Update(gameTime);
@@ -177,15 +181,32 @@ namespace Pong
 
                 }
             }
-            
+            for (int i = 0; i < tripod_pos_list.Count; i++)
+            {
+                if (tripod_pos_list[i].Y >= (Window.ClientBounds.Height + tripod.Height))
+                {
+                    tripod_pos_list.RemoveAt(i);
+                }
+            }
+
+        }
+
+        public Rectangle recta()
+        {
+            return new Rectangle(50, 50, 200, 50);
         }
         protected override void Draw(GameTime gameTime)
         {
+            
             GraphicsDevice.Clear(Color.BurlyWood);
             
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
+
             
+
+            _spriteBatch.Draw(platform, recta() , Color.Black);
+
             foreach (Vector2 cn in coin_pos_list)
             {
                 _spriteBatch.Draw(coin, cn, Color.White);
@@ -194,8 +215,9 @@ namespace Pong
             {
                 _spriteBatch.Draw(tripod, cn, Color.White);
             }
-            
 
+            
+            _spriteBatch.DrawString(gameFont, "Poäng:", new Vector2(10, 10), Color.White);
             _spriteBatch.Draw(myship, myship_pos, Color.White);
 
             _spriteBatch.End();
